832ad02cb5bcdd6c223a626503e9f1b6f462e98e
SulaimanAbdRah98 <sulaimanabdrahman98@gmail.com>
2025-07-08 13:04:35 +0800

    Added additional comments to meter animation script

--- Assets/_SCRIPTS/VictoryScreen/MeterAnimation.cs
+++ Assets/_SCRIPTS/VictoryScreen/MeterAnimation.cs
@@ -5,42 +5,57 @@ using TMPro;
 
 namespace TDS.UI.Victory
 {
+    //Here is a script that we can use for the score bar/meter
     public class MeterAnimation : MonoBehaviour
     {
         [Header("SETTINGS")]
+        [Tooltip("This value is what we would set as our target score value")]
         [SerializeField] private int targetValue;
+        [Tooltip("How long would the animation take to get to the target value")]
         [SerializeField] private float duration;
 
         [Header("COMPONENTS")]
+        [Tooltip("Our text mesh to display the score. Optional")]
         [SerializeField] private TextMeshProUGUI valueText;
+        [Tooltip("Our slider element to display the score. Optional")]
         [SerializeField] private Slider meterSlider;
 
+        [Tooltip("Unity Event to call when the animation is finished")]
         [SerializeField] private UnityEvent onComplete;
 
         private float curValue;
         private float elapsedTime;
         private bool isPlaying;
 
+        //This is how we trigger playing it
+        //Set it to true to play and set it to false to stop
         public void IsPlaying(bool playing)
         {
             isPlaying = playing;
+
+            //Maybe can implement a Stop() function if needed or a Unity Event for that
+            //There isn't any need in this case so I didn't
         }
 
         private void Update()
         {
+            //If it's playing, then we need to update its values
             if (isPlaying)
             {
                 UpdateValues();
             }
         }
 
+        //Updating the values
         private void UpdateValues()
         {
+            //Doing some math to figure out how to do the lerping
+            //so we can make sure our animation finish on the correct time
             elapsedTime += Time.deltaTime;
             float t = Mathf.Clamp01(elapsedTime / duration);
 
             
-
+            //Displaying the values on the text mesh and slider accordingly
             if(valueText != null)
             {
                 curValue = Mathf.Lerp(0, targetValue, t);
@@ -53,7 +68,7 @@ namespace TDS.UI.Victory
             }
 
 
-
+            //If t is 1, that means we have completed the animation
             if (t == 1)
             {
                 isPlaying = false;
@@ -63,6 +78,7 @@ namespace TDS.UI.Victory
 
 
 
+        //Helper function so that we can play the anim again when we trigger the victory screen
         public void ResetValues()
         {
             curValue = 0;
